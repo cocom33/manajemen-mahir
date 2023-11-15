12740f13d9c53247733d04764773de33cdf19a90
cocom33 <ancientcococ3@gmail.com>
2023-11-15 13:21:17 +0700

    keuangan

---
+++ app/Models/KeuanganBulanan.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KeuanganBulanan extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['bulan', 'keuangan_perusahaan_id'];
+
+    public function tahun()
+    {
+        return $this->belongsTo(KeuanganPerusahaan::class);
+    }
+
+    public function detail()
+    {
+        return $this->hasMany(KeuanganDetail::class);
+    }
+}
--- resources/views/admin/keuangan-perusahaan/index.blade.php
+++ resources/views/admin/keuangan-perusahaan/index.blade.php
@@ -4,7 +4,13 @@
 @section('content')
  <!-- BEGIN: Content -->
  <div class="content">
-
+    {{ $data->tahun }}
+    @foreach ($data->bulan as $item)
+        <p>{{ $item }}</p>
+        <p>{{ $item->detail }}</p>
+        <br>
+    @endforeach
+    {{-- {{ $data->bulan }} --}}
 </div>
 <!-- END: Content -->
 @endsection
