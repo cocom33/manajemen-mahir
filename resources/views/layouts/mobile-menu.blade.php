eaf96c2048d8f76e26043d3007b6979b0dd25889
Pasha Rama <113332544+rammmpr@users.noreply.github.com>
2023-12-20 20:22:39 +0700

    edit-mobile

--- resources/views/layouts/mobile-menu.blade.php
+++ resources/views/layouts/mobile-menu.blade.php
@@ -9,22 +9,34 @@
         'project' =>  [
             'title'  => 'Projects',
             'url'    => route('projects'),
-            'icon'   => 'home',
-            'name'   => 'projects',
+            'icon'   => 'briefcase',
+            'name'   => 'project',
         ],
-        'keuangan-perusahaan' =>  [
-            'title'  => 'Keuangan Perusahaan',
-            'url'    => route('keuangan-perusahaan.index'),
-            'icon'   => 'home',
-            'name'   => 'keuangan-perusahaan',
+        'tagihan' =>  [
+            'title'  => 'Tagihan',
+            'url'    => route('tagihan'),
+            'icon'   => 'alert-triangle',
+            'name'   => 'tagihan',
         ],
+        // 'keuangan-perusahaan' =>  [
+        //     'title'  => 'Keuangan Perusahaan',
+        //     'url'    => route('keuangan-perusahaan.index'),
+        //     'icon'   => 'home',
+        //     'name'   => 'keuangan-perusahaan',
+        // ],
         'keuangan-umum' =>  [
             'title'  => 'Keuangan Umum',
             'url'    => route('keuangan-umum.index'),
-            'icon'   => 'home',
+            'icon'   => 'dollar-sign',
             'name'   => 'keuangan-umum',
         ],
         'devider',
+        'teams' =>  [
+            'title'  => 'Teams',
+            'url'    => route('teams.index'),
+            'icon'   => 'users',
+            'name'   => 'teams',
+        ],
         'category-project' =>  [
             'title'  => 'Project Category',
             'url'    => route('category-project.index'),
@@ -36,12 +48,6 @@
             'url'    => route('skill.index'),
             'icon'   => 'hexagon',
             'name'   => 'skill'
-        ],
-        'team' =>  [
-            'title'  => 'Team',
-            'url'    => route('teams.index'),
-            'icon'   => 'user',
-            'name'   => 'team',
         ],
         'client' =>  [
             'title'  => 'Client',
@@ -56,25 +62,33 @@
             'icon'   => 'user',
             'name'   => 'profile',
         ],
-        'team' =>  [
-            'title'  => 'asd',
-            'icon'   => 'user',
-            'name'   => 'client',
-            'children' => [
-                [
-                    'title'  => 'tes 11',
-                    'url'    => route('client.index'),
-                    'icon'   => 'activity',
-                ],
-                [
-                    'title'  => 'tes 2',
-                    'url'    => route('client.index'),
-                    'icon'   => 'activity',
-                ],
-            ]
-        ],
+        // 'user' =>  [
+        //     'title'  => 'User',
+        //     'url'    => route('profile.edit'),
+        //     'icon'   => 'user',
+        //     'name'   => 'profile',
+        // ],
+        // 'team' =>  [
+        //     'title'  => 'asd',
+        //     'icon'   => 'user',
+        //     'name'   => 'team.index',
+        //     'children' => [
+        //         [
+        //             'title'  => 'tes 11',
+        //             'url'    => route('client.index'),
+        //             'icon'   => 'activity',
+        //             'name'   => 'client',
+        //         ],
+        //         [
+        //             'title'  => 'tes 2',
+        //             'url'    => route('client.index'),
+        //             'icon'   => 'activity',
+        //             'name'   => 'client',
+        //         ],
+        //     ]
+        // ],
     ];
-@endphp
+@endphp                                     
 
 <!-- BEGIN: Mobile Menu -->
 <div class="mobile-menu md:hidden">
@@ -92,14 +106,14 @@
                  @if (empty($menu['children']))
                     <li class="hover:bg-theme-20">
                         <a href="{{ $menu['url'] }}" class="menu menu--active hover:bg-theme-20">
-                            <div class="menu__icon"> <i data-feather="home"></i> </div>
+                            <div class="menu__icon"> <i data-feather="{{ $menu['icon'] }}"></i> </div>
                             <div class="menu__title"> {{ $menu['title'] }} </div>
                         </a>
                     </li>
                 @else
                     <li class="hover:bg-theme-20">
                         <a href="javascript:;" class="menu menu--active hover:bg-theme-20">
-                            <div class="menu__icon"> <i data-feather="home"></i> </div>
+                            <div class="menu__icon"> <i data-feather="layout"></i> </div>
                             <div class="menu__title"> {{ $menu['title'] }} <i data-feather="chevron-down" class="menu__sub-icon"></i> </div>
                         </a>
                         <ul class="">
