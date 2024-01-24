a988bb7115433e2a68e49c15818930afbd756212
Pasha Rama <113332544+rammmpr@users.noreply.github.com>
2024-01-24 10:18:31 +0700

    a

--- resources/views/admin/skill/create.blade.php
+++ resources/views/admin/skill/create.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@push('styles')
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
+@endpush
+
 @section('content')
 <div class="flex items-center mt-8 intro-y">
     <h2 class="mr-auto text-lg font-medium">Add New Skill</h2>
@@ -17,6 +21,7 @@
                         <div class="mt-2 text-theme-6">{{ $message }}</div>
                     @enderror
                 </div>
+                <div id="summernote"></div>
                 <div class="mt-5 text-right">
                     <a href="{{ route('category-project.index') }}"><button type="button" class="w-24 mr-1 text-gray-700 border button">Cancel</button></a>
                     <button type="submit" class="w-24 text-white button bg-theme-1">Save</button>
@@ -27,3 +32,24 @@
     </div>
 </div>
 @endsection
+
+@push('scripts')
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+<script>
+      $('#summernote').summernote({
+        placeholder: 'Hello stand alone ui',
+        tabsize: 2,
+        height: 120,
+        toolbar: [
+          ['style', ['style']],
+          ['font', ['bold', 'underline', 'clear']],
+          ['color', ['color']],
+          ['para', ['ul', 'ol', 'paragraph']],
+          ['table', ['table']],
+          ['insert', ['link', 'picture', 'video']],
+          ['view', ['fullscreen', 'codeview', 'help']]
+        ]
+      });
+    </script>
+@endpush
